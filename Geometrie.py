a1f444f7338572273936ce64650a19b0494c33f4
Alex Nigge <alexandranigge@gmail.com>
2025-01-24 12:44:08 +0100

    Geometrie for eigenen Ansatz
    Testklassen umbenannt und erster Test für Geometrie-Klasse

--- Geometrie.py
+++ Geometrie.py
@@ -37,3 +37,34 @@ def reflect_on_hypotenuse(arm, x0, y0, x1, y1, x2, y2):
     y_reflection = 2 * y_intersection - y0
     
     return x_reflection, y_reflection
+
+# TODO: Comment Determines, if the joint is tilted to the right (0) or to the left (1) 
+# in the perspective of the ursprung of the link
+# Input:
+#   angle
+# Output:
+#   0 if eingeschlagen to the right side
+#   1 if eingeschlagen to the left side
+def which_side_small_angle(angle):
+    if(angle % (2*np.pi) < np.pi):
+        return 0
+    else:
+        return 1
+    
+# TODO: Determines, from perspective of the coxa link, if the obstacle is on its right side or left side
+# Input:
+#   arm: To know the position and current posture of the coxa link
+#   center: Center Point of the Obstacle
+# Ouput:
+#   0 : Obstacle is on the right side of coxa TODO is this correct? Should be correct
+#   1 : Obstacle is on the left side of coxa
+def which_side_obstacle_to_coxa(arm, center):
+    # Calculate the slope of the coxa link under the presumption, that the arm's origin is always (0,0)
+    m = arm.joint_coxa[1]/arm.joint_coxa[0]
+
+    f_center = m * center[0]
+
+    if (f_center > center[1]):
+        return 0
+    else:
+        return 1
